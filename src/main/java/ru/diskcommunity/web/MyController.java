d3a82d0b75e6bbb028b6a558876872942df5ec69
MikhailForrest <treushnikovmv@ya.ru>
2017-11-13 23:03:43 +0300

    1311

--- src/main/java/ru/diskcommunity/web/MyController.java
+++ src/main/java/ru/diskcommunity/web/MyController.java
@@ -2,10 +2,16 @@ package ru.diskcommunity.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import ru.diskcommunity.domain.DisksEntity;
 import ru.diskcommunity.service.DisksService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/disks")
@@ -14,9 +20,30 @@ public class MyController {
     @Autowired
     DisksService disksService;
 
-    @RequestMapping(value="/")
+    @RequestMapping(value="", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    String home(){
-        return "ggg";
+    List<DisksEntity> getOwnDisksForUser(@RequestParam (value="user_id", required=false) Integer userId,
+                                         @RequestParam (value="is_free", required=false) Boolean isFree){
+        if (userId!=null) {
+            if (isFree!=null){
+                if (isFree=true){
+                    List<DisksEntity> listOfDisks = disksService.DisksTakenFromUser(userId);
+                    return listOfDisks;
+                }else {
+                    List<DisksEntity> listOfDisks = disksService.DisksTakenByUser(userId);
+                    return listOfDisks;
+                }
+            }
+            else
+            { List<DisksEntity> listOfDisks = disksService.OwnDisksForUser(userId);
+                return listOfDisks;}
+        } else {
+            if (isFree!=null){
+                return null;
+            }else{
+            List<DisksEntity> listOfDisks = disksService.AllFreeDisks();
+            return listOfDisks;}
+        }
+
     }
 }
